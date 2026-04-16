2daec5ff1b21ba4968b92b77c3e247a7a96f7432
Jordan4184 <jordandonaldson14@gmail.com>
2026-04-16 12:41:45 -0700

    streak.py: counts total days logged from LOG.md

--- streak.py
+++ streak.py
@@ -5,16 +5,15 @@
 import datetime as datetime
 
 with open('LOG.md', 'r', encoding = 'utf-8') as file: 
+    streak = []
     for line in file: 
         clean_line = line.strip()
         if clean_line.startswith('##'): 
-            date_str = '2026-04-14'
-            dt = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+            date_str = '## Day 0 - 2026-4-14'
+            partial_str = line.split(' - ') 
+            just_date = partial_str[-1]
+            just_date = just_date.strip()
+            dt = datetime.datetime.strptime(just_date, '%Y-%m-%d')
             extracted_date = dt.date()
-            print(extracted_date)
-
-#If date from yesterday is not equal to 0 and datetime equals today: 
-    #then increase streak counter by 1 
-#Else streak = 1 
-
-
+            streak.append(extracted_date)
+    print(f'Streak = {len(streak)}')
