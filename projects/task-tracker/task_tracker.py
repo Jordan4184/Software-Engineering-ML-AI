fc5494f170e1aa25388368c44a17103245accf8f
Jordan4184 <jordandonaldson14@gmail.com>
2026-04-23 13:53:18 -0700

    Add numbered task display with enumerate

--- projects/task-tracker/task_tracker.py
+++ projects/task-tracker/task_tracker.py
@@ -1,21 +1,21 @@
 ## CLI task manager inteded to serve as a reminder of todays tasks. Includes adding tasks, editing, and deleting tasks
 import json
 
-## Initiate list of to-do's 
+## Initiate list of to-do's
 todo_list = []
 print("Enter your tasks (type 'quit' to stop):")
 
 ## Core Logic
 while True:
     task = input("> ")
-    if task.lower() == 'quit':
+    if task.lower() == "quit":
         break
     todo_list.append(task)
 
 ## Print statement:
 print("Your To-Do List: \n")
 
-#Saving a Task to JSON
+# Saving a Task to JSON
 data = todo_list
 
 with open("data.json", "w") as file:
@@ -24,8 +24,9 @@ with open("data.json", "w") as file:
 with open("data.json", "r") as infile:
     data = json.load(infile)
 
-print(data)
-
+for i, todo in enumerate(todo_list, 1):
+    print(f"{i}. {todo}")
 ## Editing a Task
 
 ## Deleting a Task
+
